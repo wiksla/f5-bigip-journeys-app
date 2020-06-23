83b5fae5e8edb46af730415f0bd2666afc020e7f
Rafal Makagon <r.makagon@f5.com>
2020-06-23 11:56:31 +0200

    Add CLI tool to parse config files

--- migrate.py
+++ migrate.py
@@ -1,6 +1,8 @@
 import click
 import json
 
+from parser.SCFStateMachine import SCFStateMachine
+
 
 @click.group()
 def cli():
@@ -18,3 +20,21 @@ def make_migrate(guest):
     click.echo("Convert ucs to be loadable on VELOS tenant: {}".format(guest))
 
 
+@cli.command()
+@click.argument("config_file")
+def parse_config(config_file):
+    """ Parse config file and print its contents"""
+
+    with open(config_file, "r") as f:
+        buffer = f.read()
+
+    machine = SCFStateMachine(source=buffer)
+    output = machine.run()
+
+    for o in output:
+        print(str(o))
+        print('==============')
+
+
+if __name__ == '__main__':
+    cli()
