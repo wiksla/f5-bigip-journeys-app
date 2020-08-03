379c5e472e0ba4f480f1382a619326f8eccbe358
Jakub Szczepanski <j.szczepanski@f5.com>
2020-08-03 16:31:18 -0700

    Add a trunks conflict

---
+++ journeys/modifier/conflict/plugins/Trunk.py
@@ -0,0 +1,18 @@
+from journeys.config import Config
+from journeys.modifier.conflict.plugins.Plugin import TYPE_NOT_SUPPORTED
+from journeys.modifier.conflict.plugins.Plugin import Plugin
+from journeys.modifier.conflict.plugins.Plugin import find_object_with_type_match
+from journeys.modifier.dependency import DependencyMap
+
+
+class Trunks(Plugin):
+    ID: str = "TRUNK"
+    MSG_TYPE: str = TYPE_NOT_SUPPORTED
+    MSG_INFO: str = "net trunk"
+
+    def __init__(self, config: Config, dependency_map: DependencyMap):
+        super().__init__(
+            config,
+            dependency_map,
+            find_object_with_type_match(config=config, type_matcher=("net", "trunk")),
+        )
